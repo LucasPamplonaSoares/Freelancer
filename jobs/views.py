3772f2350723eb5d0985dd577425ea205ced11b2
LucasPamplonaSoares <lucasreitz25@gmail.com>
2022-04-07 16:44:17 -0300

    master

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Jobs
 
 def encontrar_jobs(request):
-    return render(request, 'encontrar_jobs.html')
+    if request.method == "GET":
+        jobs = Jobs.objects.filter(reservado=False)
+        return render(request, 'encontrar_jobs.html', {'jobs': jobs})
 
