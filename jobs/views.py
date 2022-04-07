93a51ee1febc6a0b835fd32d3e3f110771a96f5b
LucasPamplonaSoares <lucasreitz25@gmail.com>
2022-04-07 17:17:57 -0300

    master

--- jobs/views.py
+++ jobs/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from .models import Jobs
+from django.http import HttpResponse
 
 def encontrar_jobs(request):
     if request.method == "GET":
+        preco_minimo = request.GET.get('preco_minimo')
+        preco_maximo = request.GET.get('preco_maximo')
+
+        prazo_minimo = request.GET.get('prazo_minimo')
+        prazo_maximo = request.GET.get('prazo_maximo')
+
+        categoria = request.GET.get('categoria')
+
         jobs = Jobs.objects.filter(reservado=False)
         return render(request, 'encontrar_jobs.html', {'jobs': jobs})
-
