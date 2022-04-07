5a1a9a7999936e58a266c1d1483ef76fc0a23ad8
LucasPamplonaSoares <lucasreitz25@gmail.com>
2022-04-07 02:38:41 -0300

    master

--- jobs/views.py
+++ jobs/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def encontrar_jobs(request):
-    return HttpResponse('Testes')
+    return  render(request, 'encontrar_jobs.html')
 
