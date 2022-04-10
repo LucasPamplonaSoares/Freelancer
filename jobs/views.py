b45cea3aa984d496f937d5536b5da9a4d6ab5288
LucasPamplonaSoares <lucasreitz25@gmail.com>
2022-04-10 02:38:03 -0300

    master

--- jobs/views.py
+++ jobs/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from .models import Jobs
 from django.http import HttpResponse
 from datetime import datetime
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
 
 def encontrar_jobs(request):
     if request.method == "GET":
@@ -52,4 +55,31 @@ def aceitar_job(request, id):
 
 def perfil(request):
     if request.method == "GET":
-        return render(request, 'perfil.html')
+        jobs = Jobs.objects.filter(profissional=request.user)
+        return render(request, 'perfil.html', {'jobs': jobs})
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        primeiro_nome = request.POST.get('primeiro_nome')
+        ultimo_nome = request.POST.get('ultimo_nome')
+
+        usuario = User.objects.filter(username=username).exclude(id=request.user.id)
+
+        if usuario.exists():
+            messages.add_message(request, constants.ERROR, 'Já existe um usuário com esse Username')
+            return redirect('/jobs/perfil')
+
+        usuario = User.objects.filter(email=email).exclude(id=request.user.id)
+
+        if usuario.exists():
+            messages.add_message(request, constants.ERROR, 'Já existe um usuário com esse E-mail')
+            return redirect('/jobs/perfil')
+
+        
+        request.user.username = username
+        request.user.email = email
+        request.user.first_name = primeiro_nome
+        request.user.last_name = ultimo_nome
+        request.user.save()
+        messages.add_message(request, constants.SUCCESS, 'Dados alterado com sucesso')
+        return redirect('/jobs/perfil')
